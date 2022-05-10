edc4c2f07095d370aa9a01be90ca16cd523269f1
Vidall <DjangoRaijin22@gmail.com>
2022-05-09 21:15:56 -0400

    finished auto updating nav

---
+++ js/app.js
@@ -0,0 +1,65 @@
+/**
+ *
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ *
+ * Dependencies: None
+ *
+ * JS Version: ES2015/ES6
+ *
+ * JS Standard: ESlint
+ *
+ */
+
+/**
+ * Comments should be present at the beginning of each procedure and class.
+ * Great to have comments before crucial code sections within the procedure.
+ */
+
+/**
+ * Define Global Variables
+ *
+ */
+const sections = document.querySelectorAll("[data-nav]");
+const navUl = document.getElementById("navbar__list");
+
+console.log(sections);
+sections.forEach(section =>{
+  const listElement = document.createElement('li')
+  listElement.innerHTML = section.getAttribute('data-nav');
+  listElement.classList.add("menu__link");
+  navUl.appendChild(listElement);
+  
+})
+console.log(navUl)
+/**
+ * End Global Variables
+ * Start Helper Functions
+ *
+ */
+
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ *
+ */
+
+// build the nav
+
+// Add class 'active' to section when near top of viewport
+
+// Scroll to anchor ID using scrollTO event
+
+/**
+ * End Main Functions
+ * Begin Events
+ *
+ */
+
+// Build menu
+
+// Scroll to section on link click
+
+// Set sections as active

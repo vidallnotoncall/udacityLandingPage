7fd070042148808dc983fe965148d016d9ed221b
Vidall <DjangoRaijin22@gmail.com>
2022-05-10 15:39:33 -0400

    added and testing out scroll to target functionality

--- js/app.js
+++ js/app.js
@@ -26,14 +26,8 @@ const sections = document.querySelectorAll("[data-nav]");
 const navUl = document.getElementById("navbar__list");
 
 console.log(sections);
-sections.forEach(section =>{
-  const listElement = document.createElement('li')
-  listElement.innerHTML = section.getAttribute('data-nav');
-  listElement.classList.add("menu__link");
-  navUl.appendChild(listElement);
-  
-})
-console.log(navUl)
+
+
 /**
  * End Global Variables
  * Start Helper Functions
@@ -47,12 +41,32 @@ console.log(navUl)
  */
 
 // build the nav
-
+sections.forEach(section =>{
+  const listElement = document.createElement('li')
+  listElement.innerHTML = section.getAttribute('data-nav');
+  listElement.classList.add("menu__link");
+  navUl.appendChild(listElement);
+  
+})
 // Add class 'active' to section when near top of viewport
 
 // Scroll to anchor ID using scrollTO event
-
-/**
+const navItems = document.querySelectorAll('.menu__link');
+navItems.forEach(navItem => {
+  const linkName = navItem.innerHTML;
+  const anchorPoint = document.querySelector(`[data-nav="${linkName}"]`)
+  console.log(anchorPoint)
+  navItem.addEventListener('click', (e) =>{
+    console.log(e.target)
+    anchorPoint.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: 'smooth'
+    })
+  })
+})
+console.log(navItems)
+/** 
  * End Main Functions
  * Begin Events
  *

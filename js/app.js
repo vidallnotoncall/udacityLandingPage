4a44813040ace3b68e4602e09941e4376bdc6a4e
Vidall <DjangoRaijin22@gmail.com>
2022-05-11 15:38:43 -0400

    all functionality complete except bug in active nav functionality

--- js/app.js
+++ js/app.js
@@ -25,7 +25,6 @@
 const sections = document.querySelectorAll("[data-nav]");
 const navUl = document.getElementById("navbar__list");
 
-console.log(sections);
 
 
 /**
@@ -33,7 +32,31 @@ console.log(sections);
  * Start Helper Functions
  *
  */
+const buildListItems = (section) => {
+  const listElement = document.createElement('li')
+  listElement.innerHTML = section.getAttribute('data-nav');
+  listElement.classList.add("menu__link");
+  navUl.appendChild(listElement);
+}
 
+const isInView = (section) => {
+  const sectionPosition = section.getBoundingClientRect()
+  if (window.pageYOffset >= sectionPosition.top){
+    return true
+  }
+}
+
+const notInView = (section) => {
+  const sectionPosition = section.getBoundingClientRect()
+  if (window.pageYOffset >= sectionPosition.top){
+    return false;
+  }
+}
+const matchNavToSection = (nav) =>{
+  const linkName = nav.innerHTML;
+  const anchorPoint = document.querySelector(`[data-nav="${linkName}"]`)
+  return anchorPoint
+}
 /**
  * End Helper Functions
  * Begin Main Functions
@@ -42,23 +65,28 @@ console.log(sections);
 
 // build the nav
 sections.forEach(section =>{
-  const listElement = document.createElement('li')
-  listElement.innerHTML = section.getAttribute('data-nav');
-  listElement.classList.add("menu__link");
-  navUl.appendChild(listElement);
-  
+  buildListItems(section);
 })
 // Add class 'active' to section when near top of viewport
-
+document.addEventListener('scroll', (e) =>{
+    navItems.forEach(item => {
+      const currentSection = matchNavToSection(item);
+      if (isInView(currentSection)){
+        item.classList.add('your-active-class');
+      }
+      if (notInView(currentSection)){
+        item.classList.remove('your-active-class')
+      }
+    
+    // function to decide what section youre in
+  })
 // Scroll to anchor ID using scrollTO event
 const navItems = document.querySelectorAll('.menu__link');
 navItems.forEach(navItem => {
   const linkName = navItem.innerHTML;
-  const anchorPoint = document.querySelector(`[data-nav="${linkName}"]`)
+  const anchorPoint = document.querySelector(`[data-nav="${linkName}"]`);
   const anchorPosition = anchorPoint.getBoundingClientRect();
-  console.log(anchorPosition)
   navItem.addEventListener('click', (e) =>{
-    console.log(e.target)
     window.scrollTo({
       top: anchorPosition.top,
       left: anchorPosition.left,

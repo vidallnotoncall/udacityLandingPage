7907e1ddd67d92423014900dc90d2f5b36154b1e
Vidall <DjangoRaijin22@gmail.com>
2022-05-13 22:03:31 -0400

    fixed all bugs and added more helper functions

--- js/app.js
+++ js/app.js
@@ -24,20 +24,21 @@
  */
 const sections = document.querySelectorAll("[data-nav]");
 const navUl = document.getElementById("navbar__list");
-const scrollToTop = document.getElementById("scroll__top")
+const topArrow = document.getElementById("scroll__top");
 
 /**
  * End Global Variables
  * Start Helper Functions
  *
  */
+//build out the menu 
 const buildListItems = (section) => {
   const listElement = document.createElement("li");
   listElement.innerHTML = section.getAttribute("data-nav");
   listElement.classList.add("menu__link");
   navUl.appendChild(listElement);
 };
-
+//check if an element is in view
 const isInView = (section) => {
   const sectionPosition = section.getBoundingClientRect();
   if (
@@ -47,19 +48,32 @@ const isInView = (section) => {
     return true;
   }
 };
-
+//match the auto generated sections of the nav to the body sections with data navs
 const matchNavToSection = (nav) => {
   const linkName = nav.innerHTML;
   const anchorPoint = document.querySelector(`[data-nav="${linkName}"]`);
   return anchorPoint;
 };
+
+//hide any element when youre near the top of the page
 const hideOnTop = (elem) => {
-  if (window.pageYOffset <= 50){
+  if (window.pageYOffset <= 50) {
     elem.style.display = "none";
-  }else{
+  } else {
     elem.style.display = "block";
   }
-}
+};
+//use any elent to scroll to the top of the page with a click
+const scrollToTop = (elem) => {
+  elem.addEventListener("click", (e) => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "smooth",
+    });
+  });
+};
+
 /**
  * End Helper Functions
  * Begin Main Functions
@@ -70,8 +84,23 @@ const hideOnTop = (elem) => {
 sections.forEach((section) => {
   buildListItems(section);
 });
+//nav items cant be grabbed until the are created
+const navItems = document.querySelectorAll(".menu__link");
 
-// Add class 'active' to section when near top of viewport
+/*end main functions
+ *
+ *
+ * begin events
+ */
+
+
+/* Add class 'active' to section when near top of viewport
+    on each scroll remove active class to make sure no conflicts arise
+    match the nav to its section
+    check if section is in view
+    add the active class styling
+    also on scroll hide the to top arrow when your already on top
+*/
 document.addEventListener("scroll", (e) => {
   navItems.forEach((item) => {
     item.classList.remove("your-active-class");
@@ -79,15 +108,19 @@ document.addEventListener("scroll", (e) => {
     if (isInView(currentSection)) {
       item.classList.add("your-active-class");
     }
-    hideOnTop(scrollToTop);
+    hideOnTop(topArrow);
   });
 });
-// Scroll to anchor ID using scrollTO event
-const navItems = document.querySelectorAll(".menu__link");
+/* Scroll to anchor ID using scrollTO event
+    select all the newly made nav items from the menu
+    match the nav item text to the sections which were originally taken from the section data nav
+    listen to the nav items for clicks
+    using the getboundingclientrect function scroll to the location of the elements using the scroll to function
+    add scoll to top functionality to the top arrow
+*/
+
 navItems.forEach((navItem) => {
-  const linkName = navItem.innerHTML;
-  const anchorPoint = document.querySelector(`[data-nav="${linkName}"]`);
-  const anchorPosition = anchorPoint.getBoundingClientRect();
+  const anchorPosition = matchNavToSection(navItem).getBoundingClientRect();
   navItem.addEventListener("click", (e) => {
     window.scrollTo({
       top: anchorPosition.top + 10,
@@ -95,24 +128,7 @@ navItems.forEach((navItem) => {
       behavior: "smooth",
     });
   });
+  scrollToTop(topArrow);
 });
-scrollToTop.addEventListener("click", (e) =>{
-  window.scrollTo({
-    top: 0,
-    left: 0,
-    behavior: "smooth",
-  })
-})
-/**
- * End Main Functions
- * Begin Events
- *
- */
 
-// Build menu
 
-// Scroll to section on link click
-
-// Set sections as 21`
-
-//hide scroll to top button when not at top
